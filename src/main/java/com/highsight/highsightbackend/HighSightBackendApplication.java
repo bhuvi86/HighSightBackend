d9fed4fa8b1fa0e33a04c05f65bf9dbf8a70e9a8
Mani <mani154@users.noreply.github.com>
2020-02-23 15:41:54 +0530

    Tested working of Controller by Printing Time.

---
+++ src/main/java/com/highsight/highsightbackend/HighSightBackendApplication.java
@@ -0,0 +1,16 @@
+package com.highsight.highsightbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class HighSightBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HighSightBackendApplication.class, args);
+    }
+
+}
